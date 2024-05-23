976d74ce05e10de1d9e2fa5d519aea7c68e9a672
NetDevs <ryan@netdevs.net>
2024-05-23 16:25:27 -0700

    Desktop notifier model, ready for connection to a frontend.

---
+++ desktop-notifier.py
@@ -0,0 +1,27 @@
+import requests
+import xml.etree.ElementTree as ET
+
+RSS_FEED_URL = "https://www.latimes.com/feeds"
+
+def loadRSS():
+	resp = requests.get(RSS_FEED_URL)
+	return resp.content
+	
+def parseXML(rss):
+	root = ET.fromstring(rss)
+	newsitems = []
+	for item in root.findall('./channel/item'):
+		news = {}
+		for child in item:
+			if child.tag == '{http://search.yahoo.com/}content':
+				news['media'] = child.attrib['url']
+			else:
+				news[child.tag] = child.text.encode('utf8')
+		newsitems.append(news)	
+	return newsitems
+
+def topStories():
+	rss = loadRSS()
+	newsitems = parseXML(rss)
+	return newsitems
+	
